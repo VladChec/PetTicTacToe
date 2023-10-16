5f6cd1bae7b11e0e2a35411d5405772a4759b924
capsicua <darknessxemen389@gmail.com>
2023-10-16 16:27:41 +0300

    commit

---
+++ java/main/tictactoe/Main.java
@@ -0,0 +1,17 @@
+package tictactoe;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Igrulya");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setSize(800,800);
+        window.setLayout(new BorderLayout());
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        TicTacToe game = new TicTacToe();
+        window.add(game);
+    }
+}
